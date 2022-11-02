c1f1bb179d172946d5a33873db87821bb93b491e
AlexsandroGobbi <105758441+AlexsandroGobbi@users.noreply.github.com>
2022-11-01 22:29:34 -0300

    Add files via upload

---
+++ quickproblem/src/main/java/Operador.java
@@ -0,0 +1,22 @@
+public class Operador {
+    public String nome;
+    public Integer funcional;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getFuncional() {
+        return funcional;
+    }
+
+    public void setFuncional(Integer funcional) {
+        this.funcional = funcional;
+    }
+    //- Nome;
+    //- Funcional;
+}
