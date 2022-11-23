f60e7c81a2ae34a412094f4d02baab50252aba96
AlexsandroGobbi <105758441+AlexsandroGobbi@users.noreply.github.com>
2022-11-23 19:15:43 -0300

    Delete quickproblem directory

--- quickproblem/src/main/java/com/quickproblem/repository/OperadorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.quickproblem.repository;
-
-import com.quickproblem.modelo.Operador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OperadorRepository extends JpaRepository<Operador,Long> {
-    Operador findByFuncional (String funcional);
-
-}
